a347a278c83a9ffd48201e0109b0b4098c8500de
HastenX <133537562+HastenX@users.noreply.github.com>
2025-12-06 15:31:36 -0500

    Syntax fixes

--- Main.sh
+++ Main.sh
@@ -9,8 +9,8 @@ function run() {
     fi
 
 
-    read -p "Would you like encryption?(Y/n): " ecryption
-    if [[ $ecryption == "Y" ]]; then
+    read -p "Would you like encryption?(Y/n): " encryption
+    if [[ $encryption == "Y" ]]; then
         encrypt
     fi
 
@@ -27,7 +27,7 @@ function run() {
     read -p "Please enter the root password: " rootPassword
     read -p "Please enter your username: " user
     read -p "Please enter $user password: " userPassword
-    read -p "Please enter the desktop you want:(g=gnome,p=plasma,h=hyprland) " $desktop
+    read -p "Please enter the desktop you want:(g=gnome,p=plasma,h=hyprland) " desktop
 
     runChroot | arch-chroot /mnt 
 
@@ -59,25 +59,25 @@ function encrypt() {
     if [[ $isNvm == 1  ]]; then
         uuid=$(blkid | grep $disk"p3" | grep -oE '[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}' )
         cryptsetup luksFormat /dev/$disk"p3"
-        cryptstup open --type luks /dev/$disk"p3"
+        cryptsetup open --type luks /dev/$disk"p3"
     else
         uuid=$(blkid | grep $disk"3" | grep -oE '[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}' )
         cryptsetup luksFormat /dev/$disk"3"
-        cryptstup open --type luks /dev/$disk"3"
+        cryptsetup open --type luks /dev/$disk"3"
     fi
 }
 
 function partitionThird() {
     if [[ $encryption == "Y" ]]; then
-        pvcreate /dev/mapper/lvm
+        pvcreate -ff /dev/mapper/lvm
         vgcreate volgroup0 /dev/mapper/lvm
 
     else 
         if [[ $isNvm == 1 ]]; then
-            pvcreate /dev/$disk"p3"
+            pvcreate -ff /dev/$disk"p3"
             vgcreate volgroup0 /dev/$disk"p3"
         else
-            pvcreate /dev/$disk"3"
+            pvcreate -ff /dev/$disk"3"
             vgcreate volgroup0 /dev/$disk"3"
         fi
     fi
@@ -103,7 +103,7 @@ function mountParts() {
     mkdir /mnt/home
     mkdir /mnt/boot
     mount /dev/volgroup0/lv_root /mnt
-    mount /dev/volgroup0/lv_root /mnt/home
+    mount /dev/volgroup0/lv_home /mnt/home
     if [[ $isNvm == 1 ]]; then
         mount /dev/$disk"p2" /mnt/boot
     else 
@@ -115,7 +115,7 @@ function runChroot() {
     echo $rootPassword; echo $rootPassword | passwd $rootPassword
     rootPassword=0
 
-    Useradd -m -g users -G wheel $user
+    useradd -m -g users -G wheel $user
     echo $userPassword; echo $userPassword; passwd $user
     user=0
     userPassword=0
@@ -134,7 +134,7 @@ function runChroot() {
     locale-gen
 
     generateGrubFile
-    cat txt/grub/compiledGrub.txt > etc/default/grub
+    cat txt/grub/compiledGrub.txt > /etc/default/grub
 
     grub-install --target=x86_64-efi --bootloader-id=grub_uefi --recheck
     cp /usr/share/locale/en@quot/LC_MESSAGES/grub.mo /boot/grub/locale/en.mo
@@ -178,7 +178,7 @@ function setDesktop() {
 
 function generateGrubFile() {
     cat txt/grub/grubTop.txt > txt/grub/compiledGrub.txt
-    if [[ $encryption == 1 ]]; then
+    if [[ $encryption == "Y" ]]; then
         echo GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3 cryptdevice=UUID=$uuid:volgroup0 quiet" >> txt/grub/compiledGrub.txt
     else
         echo GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3 quiet" >> txt/grub/compiledGrub.txt
