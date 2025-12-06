bf68e5ee926d6f476e875fd503d6f15c70abf1f4
HastenX <133537562+HastenX@users.noreply.github.com>
2025-12-06 14:31:56 -0500

    Improved script (chroot)

--- Main.sh
+++ Main.sh
@@ -20,47 +20,12 @@ function run() {
     setDiskTypes
     mountParts
 
-    pacstrap -i /mnt base sudo nano
+    cat txt/pacstrapInput.txt | pacstrap -i /mnt base sudo nano
 
     genfstab -U -p /mnt >> /mnt/etc/fstab
 
-    arch-chroot /mnt
-    passwd
+    arch-chroot /mnt ./Chroot.sh
 
-    read -p "Please enter your username: " user
-    Useradd -m -g users -G wheel $user
-    passwd $user
-    cat txt/sudoersFile.txt > /etc/sudoers
-
-    mountBoot
-
-    echo Y | pacman -S  base-devel dosfstools grub git efibootmgr lvm2 mtools bash-completion networkmanager os-prober linux linux-headers linux-firmware mesa ufw libva-mesa-driver intel-media-drivers
-    read -p "Please enter the desktop you want:(g=gnome,p=plasma,h=hyprland) " $desktop
-    setDesktop $desktop
-
-    cat txt/mkinitcpioFile.txt > /etc/mkinitcpio.conf
-    mkinitcpio -p linux 
-
-    cat txt/localeFile.txt > /etc/locale.gen
-    locale-gen
-
-    generateGrubFile
-    cat txt/grub/compiledGrub.txt > etc/default/grub
-
-    grub-install --target=x86_64-efi --bootloader-id=grub_uefi --recheck
-    cp /usr/share/locale/en@quot/LC_MESSAGES/grub.mo /boot/grub/locale/en.mo
-    grub-mkconfig -o /boot/grub/grub.cfg
-
-    systemctl enable networkmanager
-    systemctl enable ufw
-    if [[ $desktop == "p" ]]; then
-        systemctl enable sddm
-    fi
-    if [[ $desktop == "g" ]]; then
-        systemctl enable gdm
-    fi 
-
-    exit
     umount -a
     reboot
 }
@@ -133,42 +98,6 @@ function mountParts() {
     else 
         mount /dev/$disk"2" /mnt/boot
     fi
-}
-
-function mountBoot() {
-    mkdir /boot/EFI
-    if [[ $isNvm == 1 ]]; then
-        mount /dev/$disk"p1" /boot/EFI
-    else
-        mount /dev/$disk"1" /boot/EFI
-    fi
-}
-
-function setDesktop() {
-    if [[ $1 == "g" ]]; then
-        echo Y | pacman -S gnome-desktop gdm
-        return
-    fi
-    if [[ $1 == "p" ]]; then
-        echo Y | pacman -S plasma-desktop sddm
-        return
-    fi
-    if [[ $1 == "h" ]]; then
-        echo Y | pacman -S hyprland
-        return
-    fi
-    echo "None anwsers recieved, default to plasma:"
-    echo Y | pacman -S plasma-desktop sddm
-}
-
-function generateGrubFile() {
-    cat txt/grub/grubTop.txt > txt/grub/compiledGrub.txt
-    if [[ $encryption == 1 ]]; then
-        echo GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3 cryptdevice=UUID=$uuid:volgroup0 quiet" >> txt/grub/compiledGrub.txt
-    else
-        echo GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3 quiet" >> txt/grub/compiledGrub.txt
-    fi
-    cat txt/grub/grubBottom.txt >> txt/grub/compiledGrub.txt
 }
 
 run
