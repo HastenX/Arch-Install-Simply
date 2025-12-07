cdcb42f1af10023ca18cffbf8dba25f66a0a13fd
HastenX <133537562+HastenX@users.noreply.github.com>
2025-12-07 12:38:29 -0500

    fixing arch-chroot system

--- Main.sh
+++ Main.sh
@@ -38,7 +38,6 @@ function run() {
 
     genfstab -U -p /mnt >> /mnt/etc/fstab
 
-    # export sudoersFile="$(cat txt/sudoersFile.txt)"
     export sudoersFile="$(cat txt/sudoersFile.txt)" 
     export mkinitcpioFile="$(cat txt/mkinitcpioFile.txt)"
     export localeFile="$(cat txt/localeFile.txt)"
@@ -56,7 +55,21 @@ function run() {
     # echo "isNvm:$isNvmVar,disk:$diskVar,uuid:$uuidVar"
     # read -p "Vars: " wait
 
-    arch-chroot /mnt bash -c "$(declare -f runChroot); runChroot \
+    # arch-chroot /mnt bash -c "$(declare -f runChroot); runChroot \
+    # "$(printf '%q' "$sudoersFile")" \
+    # "$(printf '%q' "$mkinitcpioFile")" \
+    # "$(printf '%q' "$localeFile")" \
+    # "$(printf '%q' "$grubTopFile")" \
+    # "$(printf '%q' "$grubBottomFile")" \
+    # "$(printf '%q' "$desktopVar")" \
+    # "$(printf '%q' "$userVar")" \
+    # "$(printf '%q' "$userPasswordVar")" \
+    # "$(printf '%q' "$rootPasswordVar")" \
+    # "$(printf '%q' "$isNvmVar")" \
+    # "$(printf '%q' "$diskVar")" \
+    # "$(printf '%q' "$encryptionVar")" \
+    # "$(printf '%q' "$uuidVar")""
+    runChroot \
     "$(printf '%q' "$sudoersFile")" \
     "$(printf '%q' "$mkinitcpioFile")" \
     "$(printf '%q' "$localeFile")" \
@@ -69,7 +82,7 @@ function run() {
     "$(printf '%q' "$isNvmVar")" \
     "$(printf '%q' "$diskVar")" \
     "$(printf '%q' "$encryptionVar")" \
-    "$(printf '%q' "$uuidVar")""
+    "$(printf '%q' "$uuidVar")"
 
     if [[ $isNvm == 1 ]]; then
         {
@@ -168,93 +181,90 @@ function mountParts() {
 }
 
 function runChroot() {
-    # mkdir "/etc/storeRes"
-    # touch "/etc/storeRes/one"
-    # touch "/etc/storeRes/two"
-    # touch "/etc/storeRes/three"
-    # touch "/etc/storeRes/four"  
-    # touch "/etc/storeRes/five"
-    # touch "/etc/storeRes/six"
-    # touch "/etc/storeRes/seven"
-    # touch "/etc/storeRes/eight"  
-    # touch "/etc/storeRes/nine"
-    # touch "/etc/storeRes/ten"
-    # touch "/etc/storeRes/eleven"
-    # touch "/etc/storeRes/twelve"
-    # touch "/etc/storeRes/thirteen"
-    # echo "$1" > "/etc/storeRes/one" #about only half of file
-    # echo "$2" > "/etc/storeRes/two" #only the word "to"
-    # echo "$3" > "/etc/storeRes/three" #only the word "find "
-    # echo "$4" > "/etc/storeRes/four" #only the word "commands."
-    # echo "$5" > "/etc/storeRes/five" #nothing
-    # echo "$6" > "/etc/storeRes/six" #nothing
-    # echo "$7" > "/etc/storeRes/seven" #nothing
-    # echo "$8" > "/etc/storeRes/eight"  #nothing
-    # echo "$9" > "/etc/storeRes/nine" #nothing
-    # echo "${10}" > "/etc/storeRes/ten" #nothing
-    # echo "${11}" > "/etc/storeRes/eleven" #nothing
-    # echo "${12}" > "/etc/storeRes/twelve" #nothing
-    # echo "${13}" > "/etc/storeRes/thirteen" #nothing
+    arch-chroot /mnt bash -c "
+    mkdir "/etc/storeRes"
+    touch "/etc/storeRes/one"
+    touch "/etc/storeRes/two"
+    touch "/etc/storeRes/three"
+    touch "/etc/storeRes/four"  
+    touch "/etc/storeRes/five"
+    touch "/etc/storeRes/six"
+    touch "/etc/storeRes/seven"
+    touch "/etc/storeRes/eight"  
+    touch "/etc/storeRes/nine"
+    touch "/etc/storeRes/ten"
+    touch "/etc/storeRes/eleven"
+    touch "/etc/storeRes/twelve"
+    touch "/etc/storeRes/thirteen"
+    echo "$sudoersFile" > "/etc/storeRes/one" 
+    echo "$mkinitcpioFile" > "/etc/storeRes/two"
+    echo "$localeFile" > "/etc/storeRes/three" 
+    echo "$grubTop" > "/etc/storeRes/four" 
+    echo "$grubBottomFile" > "/etc/storeRes/five" 
+    echo "$desktop" > "/etc/storeRes/six" 
+    echo "$userVar" > "/etc/storeRes/seven" 
+    echo "$userPasswordVar" > "/etc/storeRes/eight"  
+    echo "$rootPasswordVar" > "/etc/storeRes/nine" 
+    echo "$isNvmVar" > "/etc/storeRes/ten" 
+    echo "$diskVar" > "/etc/storeRes/eleven" 
+    echo "$encryptionVar" > "/etc/storeRes/twelve" 
+    echo "$uuidVar" > "/etc/storeRes/thirteen"
+    "
 
-    echo "root:$9" | chpasswd 
-    rootPassword=0
+    arch-chroot /mnt bash -c "echo "root:$rootPasswordVar" | chpasswd "
+    rootPasswordVar=0
 
-    useradd -m -g users -G wheel "$7"
-    echo "$7:$8" | chpasswd  
-    user=0
-    userPassword=0
+    arch-chroot /mnt bash -c "useradd -m -g users -G wheel "$7""
+    arch-chroot /mnt bash -c "echo "$userVar:$userPasswordVar" | chpasswd "
+    userVar=0
+    userPasswordVar=0
 
-    echo "$1" > "/etc/sudoers"
+    arch-chroot /mnt bash -c "echo "$sudoersFile" > /etc/sudoers"
 
-    mkdir "/boot/EFI"
-    if [[ "${10}" == 1 ]]; then
-        mount "/dev/"${11}"p1" "/boot/EFI"
+    arch-chroot /mnt bash -c "mkdir "/boot/EFI""
+    if [[ "$isNvmVar" == 1 ]]; then
+        arch-chroot /mnt bash -c "mount /dev/$diskVar'p1' /boot/EFI"
     else
-        mount "/dev/"${11}"1" "/boot/EFI"
+       arch-chroot /mnt bash -c "mount /dev/$diskVar'1' /boot/EFI"
     fi
-    pacman -Syy --noconfirm grub git intel-media-drivers
-    pacman -Syy --noconfirm mkinitcpio base-devel dosfstools 
-    pacman -Syy --noconfirm efibootmgr mtools linux
-    pacman -Syy --noconfirm networkmanager os-prober bash-completion
-    pacman -Syy --noconfirm linux-headers linux-firmware mesa 
-    pacman -Syy --noconfirm ufw libva-mesa-driver lvm2
-    if [[ "$6" == "g" ]]; then
-        pacman -Syy --noconfirm gnome-desktop gdm
-        systemctl enable gdm
+    arch-chroot /mnt bash -c "pacman -Syy --noconfirm grub git intel-media-drivers; pacman -Syy --noconfirm mkinitcpio base-devel dosfstools; pacman -Syy --noconfirm efibootmgr mtools linux; pacman -Syy --noconfirm networkmanager os-prober bash-completion; pacman -Syy --noconfirm linux-headers linux-firmware mesa; pacman -Syy --noconfirm ufw libva-mesa-driver lvm2"
+    if [[ "$desktop" == "g" ]]; then
+        arch-chroot /mnt bash -c "pacman -Syy --noconfirm gnome-desktop gdm"
+        arch-chroot /mnt bash -c "systemctl enable gdm"
     fi
-    if [[ "$6" == "p" ]]; then
-        pacman -Syy --noconfirm plasma-desktop sddm
-        systemctl enable sddm
+    if [[ "$desktop" == "p" ]]; then
+        arch-chroot /mnt bash -c "pacman -Syy --noconfirm plasma-desktop sddm"
+        arch-chroot /mnt bash -c "systemctl enable sddm"
     fi
-    if [[ "$6" == "h" ]]; then
-        pacman -Syy --noconfirm hyprland
+    if [[ "$desktop" == "h" ]]; then
+        arch-chroot /mnt bash -c "pacman -Syy --noconfirm hyprland"
     fi
-    if [[ "$6" != "g" && "$6" != "p" && "$6" != "h" ]]; then
+    if [[ "$desktop" != "g" && "$desktop" != "p" && "$desktop" != "h" ]]; then
         echo "None anwsers recieved, default to plasma:"
-        pacman -Syy --noconfirm plasma-desktop sddm
-        systemctl enable sddm
+        arch-chroot /mnt bash -c "pacman -Syy --noconfirm plasma-desktop sddm"
+        arch-chroot /mnt bash -c "systemctl enable sddm"
     fi
 
-    echo "$2" > "/etc/mkinitcpio.conf"
-    mkinitcpio -p linux 
+    arch-chroot /mnt bash -c "echo "$mkinitcpioFile" > /etc/mkinitcpio.conf"
+    arch-chroot /mnt bash -c "mkinitcpio -p linux"
 
-    echo "$3" > "/etc/locale.gen"
-    locale-gen
+    arch-chroot /mnt bash -c "echo "$localeFile" > /etc/locale.gen"
+    arch-chroot /mnt bash -c "locale-gen"
 
-    echo "$4" > "/etc/default/grub"
+    arch-chroot /mnt bash -c "echo "$grubTopFile" > /etc/default/grub"
     if [[ ${12} == "Y" ]]; then
-        echo GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3 cryptdevice=UUID=${13}:volgroup0 quiet" >> "/etc/default/grub"
+        arch-chroot /mnt bash -c "echo GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3 cryptdevice=UUID=$uuid:volgroup0 quiet" >> "/etc/default/grub""
     else
-        echo GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3 quiet" >> "/etc/default/grub"
+        arch-chroot /mnt bash -c "echo GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3 quiet" >> "/etc/default/grub""
     fi
-    echo "$5" >> "/etc/default/grub"
+    arch-chroot /mnt bash -c "echo "$5" >> "/etc/default/grub""
 
-    grub-install --target=x86_64-efi --bootloader-id=grub_uefi --recheck
-    cp "/usr/share/locale/en@quot/LC_MESSAGES/grub.mo" "/boot/grub/locale/en.mo"
-    grub-mkconfig -o "/boot/grub/grub.cfg"
+    arch-chroot /mnt bash -c "grub-install --target=x86_64-efi --bootloader-id=grub_uefi --recheck"
+    arch-chroot /mnt bash -c "cp "/usr/share/locale/en@quot/LC_MESSAGES/grub.mo" "/boot/grub/locale/en.mo""
+    arch-chroot /mnt bash -c "grub-mkconfig -o "/boot/grub/grub.cfg""
 
-    systemctl enable NetworkManager
-    systemctl enable ufw
+    arch-chroot /mnt bash -c "systemctl enable NetworkManager"
+    arch-chroot /mnt bash -c "systemctl enable ufw"
 }
 
 run
