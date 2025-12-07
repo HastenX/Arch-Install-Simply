341c61c0bfb7cfc5316d99ef851142d9c7783e9d
HastenX <133537562+HastenX@users.noreply.github.com>
2025-12-07 16:15:43 -0500

    Update Main.sh

--- Main.sh
+++ Main.sh
@@ -262,11 +262,11 @@ function runChroot() {
 
     echo "$4" > "/etc/default/grub"
     if [[ ${12} == "Y" ]]; then
-        "echo GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3 cryptdevice=${13}:volgroup0 quiet"" >> "/etc/default/grub"
+        echo GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3 cryptdevice=${13}:volgroup0 quiet" >> "/etc/default/grub"
     else
-        "echo GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3 quiet"" >> "/etc/default/grub"
+        echo GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3 quiet" >> "/etc/default/grub"
     fi
-    echo "GRUB_PRELOAD_MODULES="part_gpt part_msdos"" >> "/etc/default/grub"
+    echo GRUB_PRELOAD_MODULES="part_gpt part_msdos" >> "/etc/default/grub"
     echo "$5" >> "/etc/default/grub"
 
     grub-install --target=x86_64-efi --bootloader-id=grub_uefi --recheck
